7a04aae71e7b28e96167984f13362b39b1773661
irvil <ivn55villanueva@gmail.com>
2020-08-19 22:26:36 +1000

    100%

--- Lab Week 4 - Activity/Assets/PrintAndHide.cs
+++ Lab Week 4 - Activity/Assets/PrintAndHide.cs
@@ -6,16 +6,27 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i;
     public Renderer rend;
+    private int randNum;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 0;
+        rend.enabled = true;
+        randNum = Random.Range(199, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(this.gameObject.name + ": " + i++);
+
+        if (this.gameObject.tag == "Red" && i == 100){
+            this.gameObject.SetActive(false);
+        }
+
+        if (this.gameObject.tag == "Blue" && i == randNum){
+            rend.enabled = false;
+        }
     }
 }
